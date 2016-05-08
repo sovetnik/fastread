a91a54dcf4e510188e990a479b8b0a96ba776a77
sovetnik <sovetnik@MacBook.local>
2016-05-08 04:40:11 +0300

    Split handle strategy
    handle is strategy, and concrete realisation is private methods.
    deleted dummy test

--- lib/fastread/handler.rb
+++ lib/fastread/handler.rb
@@ -1,11 +1,50 @@
-require 'telegram/bot'
 class Handler
   def self.handle(bot, message)
+    new(bot, message).handle
+  end
+
+  attr_reader :bot, :message
+
+  def initialize(bot, message)
+    @bot = bot
+    @message = message
+  end
+
+  def handle
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      start
+      p 'chat started'
     when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+      stop
+      p 'chat stopped'
+    else
+      parse
+      p 'message parsed'
     end
+  end
+
+  private
+
+  def start
+    bot.api.send_message(
+      chat_id: message.chat.id,
+      text: "Hello, #{message.from.first_name}"
+    )
+  end
+
+  def stop
+    bot.api.send_message(
+      chat_id: message.chat.id,
+      text: "Fuck off, #{message.from.first_name}"
+    )
+  end
+
+  def parse
+    bot.api.send_message(
+      chat_id: message.chat.id,
+      text: 'If i had brains, i will give you an answer.'
+    )
+    p ('Need Parse: ' + message.text)
   end
 end
